a2a2d5fd9c0afe9238710e31d54e6a79644cd8f1
Chatas Chairin <cchairin@manaosoftware.com>
2024-06-15 09:23:14 +0700

    Implement SaveDataManager

---
+++ scripts/SaveDataManager.cs
@@ -0,0 +1,104 @@
+using Godot;
+using System;
+using System.IO;
+using Dictionary = Godot.Collections.Dictionary;
+
+public partial class SaveDataManager : Node2D
+{
+	private string _path, _fileName;
+
+	public override void _Ready()
+	{
+		_path = ProjectSettings.GlobalizePath("user://");
+		_fileName = "SaveGame.json";
+	}
+
+	public void SaveData(SaveFile saveFile)
+	{
+		if (!Directory.Exists(_path))
+		{
+			Directory.CreateDirectory(_path);
+		}
+
+		var filePath = Path.Join(_path, _fileName);
+
+		GD.Print(filePath);
+
+		try
+		{
+			File.WriteAllText(filePath, saveFile.ToString());
+		}
+		catch (Exception ex)
+		{
+			GD.PrintErr(ex);
+		}
+	}
+
+	public SaveFile LoadData()
+	{
+		var filePath = Path.Join(_path, _fileName);
+		if (!File.Exists(filePath))
+		{
+			return new SaveFile();
+		}
+
+		try
+		{
+			var loadedData = File.ReadAllText(filePath);
+			Json jsonLoader = new Json();
+			Error error = jsonLoader.Parse(loadedData);
+			if (error != Error.Ok)
+			{
+				GD.PrintErr(error);
+				return new SaveFile();
+			}
+
+			return new SaveFile((Dictionary)jsonLoader.Data);
+		}
+		catch (Exception ex)
+		{
+			GD.PrintErr(ex);
+		}
+
+		return new SaveFile();
+	}
+}
+
+public class SaveFile
+{
+	private Dictionary _dictionary;
+
+	public SaveFile()
+	{
+		_dictionary = new Dictionary();
+	}
+
+	public SaveFile(Dictionary dictionary)
+	{
+		_dictionary = dictionary;
+	}
+
+	public int HighScore
+	{
+		get
+		{
+			return _dictionary.ContainsKey(nameof(HighScore)) ? (int)_dictionary[nameof(HighScore)] : 0;
+		}
+		set
+		{
+			if (_dictionary.ContainsKey(nameof(HighScore)))
+			{
+				_dictionary[nameof(HighScore)] = value;
+			}
+			else
+			{
+				_dictionary.Add(nameof(HighScore), value);
+			}
+		}
+	}
+
+	public override string ToString()
+	{
+		return Json.Stringify(_dictionary);
+	}
+}
--- scripts/ShowScoreScene.cs
+++ scripts/ShowScoreScene.cs
@@ -7,8 +7,19 @@ public partial class ShowScoreScene : CanvasLayer
 
 	public override void _Ready()
 	{
+		var saveManager = GetNode<SaveDataManager>("/root/SaveDataManager");
+		var data = saveManager.LoadData();
+
+		var newScore = Convert.ToInt32(_score);
+		if (newScore > data.HighScore)
+		{
+			data.HighScore = newScore;
+			saveManager.SaveData(data);
+		}
+
 		var highScoreLabel = GetNode<Label>("Panel/MarginContainer/VBoxContainer/HBoxContainer/HighScoreLabel");
-		
+		highScoreLabel.Text = data.HighScore.ToString();
+
 		var scoreLabel = GetNode<Label>("Panel/MarginContainer/VBoxContainer/ScoreLabel");
 		scoreLabel.Text = _score;
 
